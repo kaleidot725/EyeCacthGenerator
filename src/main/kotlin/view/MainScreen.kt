bd3d359aa2ce129d2b0499e6d984b4a70881e515
kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2024-09-07 14:00:28 +0900

    feat: add split layout

--- src/main/kotlin/view/MainScreen.kt
+++ src/main/kotlin/view/MainScreen.kt
@@ -1,15 +1,12 @@
 package view
 
 import androidx.compose.desktop.ui.tooling.preview.Preview
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import org.jetbrains.jewel.foundation.theme.JewelTheme
+import org.jetbrains.jewel.ui.component.HorizontalSplitLayout
 import view.component.image.ImagePreview
 import view.component.image.editor.ImageEditor
 
@@ -19,42 +16,38 @@ fun MainScreen(
     onEvent: (MainEvent) -> Unit,
     modifier: Modifier = Modifier,
 ) {
-    Row(
+    HorizontalSplitLayout(
+        first = {
+            ImagePreview(
+                file = state.previewFile,
+                date = state.previewUpdate,
+                isLoading = state.isLoading,
+                modifier =
+                    Modifier.fillMaxHeight().fillMaxWidth(),
+            )
+        },
+        second = {
+            ImageEditor(
+                parameters = state.parameters,
+                onChangedTitle = { onEvent(MainEvent.ChangeTitle(it)) },
+                onChangedTitleFont = { onEvent(MainEvent.ChangeTitleFont(it)) },
+                onChangedTitleSize = { onEvent(MainEvent.ChangeTitleSize(it)) },
+                onChangedSubTitle = { onEvent(MainEvent.ChangeSubTitle(it)) },
+                onChangedSubTitleFont = { onEvent(MainEvent.ChangeSubTitleFont(it)) },
+                onChangedSubTitleSize = { onEvent(MainEvent.ChangeSubTitleSize(it)) },
+                onChangedWidth = { onEvent(MainEvent.ChangeWidth(it)) },
+                onChangedHeight = { onEvent(MainEvent.ChangeHeight(it)) },
+                onChangedStartColor = { onEvent(MainEvent.ChangeStartColor(it)) },
+                onChangedEndColor = { onEvent(MainEvent.ChangeEndColor(it)) },
+                onSave = { onEvent(MainEvent.Save) },
+                allFonts = state.fonts,
+                modifier = Modifier.fillMaxHeight(),
+            )
+        },
+        firstPaneMinWidth = 1000.dp,
+        secondPaneMinWidth = 400.dp,
         modifier = modifier,
-    ) {
-        ImagePreview(
-            file = state.previewFile,
-            date = state.previewUpdate,
-            isLoading = state.isLoading,
-            modifier =
-                Modifier
-                    .fillMaxHeight()
-                    .weight(1.0f),
-        )
-        Spacer(
-            modifier =
-                Modifier
-                    .fillMaxHeight()
-                    .width(1.dp)
-                    .background(JewelTheme.globalColors.borders.normal),
-        )
-        ImageEditor(
-            parameters = state.parameters,
-            onChangedTitle = { onEvent(MainEvent.ChangeTitle(it)) },
-            onChangedTitleFont = { onEvent(MainEvent.ChangeTitleFont(it)) },
-            onChangedTitleSize = { onEvent(MainEvent.ChangeTitleSize(it)) },
-            onChangedSubTitle = { onEvent(MainEvent.ChangeSubTitle(it)) },
-            onChangedSubTitleFont = { onEvent(MainEvent.ChangeSubTitleFont(it)) },
-            onChangedSubTitleSize = { onEvent(MainEvent.ChangeSubTitleSize(it)) },
-            onChangedWidth = { onEvent(MainEvent.ChangeWidth(it)) },
-            onChangedHeight = { onEvent(MainEvent.ChangeHeight(it)) },
-            onChangedStartColor = { onEvent(MainEvent.ChangeStartColor(it)) },
-            onChangedEndColor = { onEvent(MainEvent.ChangeEndColor(it)) },
-            onSave = { onEvent(MainEvent.Save) },
-            allFonts = state.fonts,
-            modifier = Modifier.fillMaxHeight().width(400.dp),
-        )
-    }
+    )
 }
 
 @Preview
