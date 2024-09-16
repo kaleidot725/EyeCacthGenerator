324c531c151d4a68f21b40c2ab3a44e7be2bfbba
kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2024-09-16 21:53:18 +0900

    feat: implement save to desktop

--- src/main/kotlin/repository/ImageRepository.kt
+++ src/main/kotlin/repository/ImageRepository.kt
@@ -12,22 +12,46 @@ import java.awt.image.BufferedImage
 import java.io.File
 import java.io.IOException
 import java.nio.file.Files
+import java.nio.file.Path
+import java.util.Date
 import javax.imageio.ImageIO
 
 class ImageRepository {
-    private val outputDirectory = File(OSContext.resolveOSContext().directory, "temp")
-    private val outputFile = File(outputDirectory, "preview.png")
+    suspend fun createPreview(parameters: Parameters): File {
+        return withContext(Dispatchers.IO) {
+            val previewDirectory = File(OSContext.resolveOSContext().directory, "temp")
+            val previewFile = File(previewDirectory, "preview.png")
+            createImage(
+                directoryPath = previewDirectory.toPath(),
+                filePath = previewFile.toPath(),
+                parameters = parameters
+            ) ?: File("")
+        }
+    }
 
-    suspend fun createPreview(parameters: Parameters): File =
+    suspend fun createFileToDesktop(parameters: Parameters): Boolean {
+        return withContext(Dispatchers.IO) {
+            val desktopDirectory = File(System.getProperty("user.home"), "Desktop")
+            val desktopFile = File(desktopDirectory, "output_${Date().time}.png")
+            val outputFile = createImage(
+                directoryPath = desktopDirectory.toPath(),
+                filePath = desktopFile.toPath(),
+                parameters = parameters
+            )
+            outputFile != null
+        }
+    }
+
+    suspend fun createImage(directoryPath: Path, filePath: Path, parameters: Parameters): File? =
         withContext(Dispatchers.IO) {
             try {
-                Files.createDirectories(outputDirectory.toPath())
+                Files.createDirectories(directoryPath)
             } catch (e: Exception) {
                 println(e)
             }
 
             try {
-                Files.delete(outputFile.toPath())
+                Files.delete(filePath)
             } catch (e: Exception) {
                 println(e)
             }
@@ -49,6 +73,7 @@ class ImageRepository {
                             parameters.height ?: 0,
                             BufferedImage.TYPE_INT_ARGB,
                         )
+
                     val imageGraphic = output.createGraphics()
                     imageGraphic.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON)
                     imageGraphic.paint =
@@ -83,17 +108,15 @@ class ImageRepository {
                     imageGraphic.font = subTitleFont
                     imageGraphic.drawString(parameters.subTitle.text, subTitleX, subTitleY)
 
-                    ImageIO.write(output, "PNG", outputFile)
-                    outputFile
+                    ImageIO.write(output, "PNG", filePath.toFile())
+                    filePath.toFile()
                 } catch (e: IOException) {
-                    outputFile
+                    null
                 }
             } else {
-                File("")
+                null
             }
         }
-
-    suspend fun createFileToDesktop(parameters: Parameters): Boolean = false
 
     private fun Color.toAwtColor(): java.awt.Color = java.awt.Color(red, green, blue, alpha)
 
@@ -111,14 +134,4 @@ class ImageRepository {
         val imageGraphic = output.createGraphics().apply { this.font = font }
         return imageGraphic.fontMetrics.stringWidth(text).apply { imageGraphic.dispose() }
     }
-
-    private fun getCenterX(
-        maxWidth: Int,
-        textWidth: Int,
-    ): Int = maxWidth / 2 - textWidth / 2
-
-    private fun getCenterY(
-        maxHeight: Int,
-        textHeight: Int,
-    ): Int = maxHeight / 2 - textHeight / 2
 }
